39482c27d4cd17af21f21d98f2827fb47c3e8c39
kmiller018 <77818949+kmiller018@users.noreply.github.com>
2021-01-30 23:01:18 -0700

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -1,14 +1,26 @@
 #!/usr/bin/env python
 import sys
+import string
+import nltk
+from nltk.corpus import stopwords
 
 # get all lines from stdin
 for line in sys.stdin:
     # remove leading and trailing whitespace
     line = line.strip()
+    
+    #make lowercase
+    line = line.lower()
+    
+    #remove punctuation
+    line = line.translate(None, string.punctuation)
 
     # split the line into words; splits on any whitespace
     words = line.split()
 
     # output tuples (word, 1) in tab-delimited format
+    stop_words = set(stopwords.words('english'))
+    
     for word in words:
-        print '%s\t%s' % (word, "1")
+        for word not in stop_words:
+            print '%s\t%s' % (word, "1")
